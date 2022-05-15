9baf93eb5fd52f7298b4c1a564ddd4914f156c51
Bartek Solarski <bartsol21@gmail.com>
2022-05-15 12:23:55 +0200

    uporzadkowanie dodanie zabezpieczenia przed zmiana nazwy samego skryptu

--- walidator.py
+++ walidator.py
@@ -1,26 +1,57 @@
 import os
 import re
 
-list = []
-localization = 'C:/Users/barts/Desktop/python/v2/walidator_plikow/test'
-# C:/Users/barts/Desktop/python/v2/walidator_plikow/test
-# ./test
+# sprawdza pliki oraz foldery z tej samej lokalizacji co jest odpalany skrypt
+# zwraca liste niedopasowana do wzorca
+def check():
+    list = []
 
-print('Skrypt odpalany z lokalizacji: ', os.getcwd())
-print('Sprawdzam pliki dla lokalizacji: ', localization, end = '\n\n')
+    print('Skrypt odpalany z lokalizacji: ', os.getcwd())
+    print('Sprawdzam pliki dla lokalizacji: ', os.getcwd(), end='\n\n')
 
-for file in os.listdir(localization):
-    valid_file = re.fullmatch("[a-zA-Z0-9._]+",file)
-    if valid_file is None:
-        list.append(file)
+    for file in os.listdir(os.getcwd()):
 
-print('Niezgodnych elementów:', len(list))
-print('\n{:20s} {:10s}'.format('Nazwa' , 'Typ'))
-print('-------------------------')
+        # walidacja za pomoca wyrazenia regularnego. Edytuj tutaj aby zmienic wzorzec.
+        valid_file = re.fullmatch("[a-zA-Z0-9._-]+", file)
+        if valid_file is None:
+            list.append(file)
 
-for file in list:
-    if os.path.isfile(localization + '/' + file):
-        type = 'plik'
-    else:
-        type = 'katalog'
-    print('{:20s} {:10s}'.format(file , type) )
+    print('Niezgodnych elementów:', len(list))
+    print('\n{:20s} {:10s}'.format('Nazwa', 'Typ'))
+    print('-------------------------')
+
+    # sprawdzenie czy plik/katalog. Dalej wyswietl w tabelce
+    for file in list:
+        if os.path.isfile(os.getcwd() + '/' + file):
+            type = 'plik'
+        else:
+            type = 'katalog'
+        print('{:20s} {:10s}'.format(file, type))
+
+    answer = input('\nCzy naprawic pliki? [tak - jesli jestes pewien]: ')
+    answer = answer.lower().strip()
+
+    return list, answer
+
+def repair(to_rapair):
+    print("tu beda naprawiane pliki")
+    print(to_rapair)
+
+##################### główny program ########################
+
+to_rapair, answer = check()
+
+if answer == 'tak':
+
+    # na wszelki wypadek - gdyby nazwa samego skryptu naprawiajacego byla niepoprawna
+    # wtedy wyklucz z listy
+    if os.path.basename(__file__) in to_rapair:
+        to_rapair.remove(os.path.basename(__file__))
+
+    repair(to_rapair)
+else:
+    print('Dziękuję. Do zobaczenia.')
+
+
+
+
