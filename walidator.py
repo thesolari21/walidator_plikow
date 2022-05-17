3341c6184b6fbc5cd1e4099f562ffb388a009c35
Bartek Solarski <bartsol21@gmail.com>
2022-05-18 00:08:45 +0200

    + dodanie funkcji translate + dodanie funkcji repair + uporządkowanie

--- walidator.py
+++ walidator.py
@@ -2,8 +2,8 @@ import os
 import re
 
 # sprawdza pliki oraz foldery z tej samej lokalizacji co jest odpalany skrypt
-# zwraca liste niedopasowana do wzorca
-def check():
+# zwraca liste plików niedopasowana do wzorca
+def check(REGEX):
     list = []
 
     print('Skrypt odpalany z lokalizacji: ', os.getcwd())
@@ -11,47 +11,86 @@ def check():
 
     for file in os.listdir(os.getcwd()):
 
-        # walidacja za pomoca wyrazenia regularnego. Edytuj tutaj aby zmienic wzorzec.
-        valid_file = re.fullmatch("[a-zA-Z0-9._-]+", file)
+        valid_file = re.fullmatch(REGEX, file)
         if valid_file is None:
             list.append(file)
 
-    print('Niezgodnych elementów:', len(list))
-    print('\n{:20s} {:10s}'.format('Nazwa', 'Typ'))
-    print('-------------------------')
+    return list
+
+# zwraca listę z poprawnymi nazwami plików
+def translate(to_rapair, DICT_TRANSLATE, REGEX):
+    translated = []
+
+    for file in to_rapair:
+        result = ''
+        for char in file:
+            # jesli litera jest w słowniku - podmien ją
+            if char in DICT_TRANSLATE:
+                result += DICT_TRANSLATE[char]
+
+            # jesli znaku nie ma w słowniku, a nie jest zgodny z regex - usun go
+            elif re.fullmatch(REGEX, char) is None:
+                result += ''
+            else:
+                result += char
+        translated.append(result)
+
+    print('Niezgodnych elementów:', len(to_rapair))
+    print('\n{:20s} {:20s} {:20s}'.format('Nazwa', 'Nowa nazwa', 'Typ'))
+    print('------------------------------------------------------------')
 
     # sprawdzenie czy plik/katalog. Dalej wyswietl w tabelce
-    for file in list:
+    for file, trans in zip(to_rapair, translated):
         if os.path.isfile(os.getcwd() + '/' + file):
             type = 'plik'
         else:
             type = 'katalog'
-        print('{:20s} {:10s}'.format(file, type))
+        print('{:20s} {:20s} {:20s}'.format(file, trans, type))
 
-    answer = input('\nCzy naprawic pliki? [tak - jesli jestes pewien]: ')
-    answer = answer.lower().strip()
+    return translated
 
-    return list, answer
+# zmienia nazwy plikow korzystajac z list to_repair, translated
+def repair(to_repair, translated):
+    print(to_repair)
+    print(translated)
 
-def repair(to_rapair):
-    print("tu beda naprawiane pliki")
-    print(to_rapair)
+    for file, trans in zip(to_repair, translated):
+        old = to_rapair
+        new = translated
+        os.rename(old,new)
 
 ##################### główny program ########################
 
-to_rapair, answer = check()
+# wyrażenie regularne na podstawie którego są sprawdzane pliki
+REGEX = '[a-zA-Z0-9._-]+'
 
+# słownik translacji
+DICT_TRANSLATE = {'ą' : 'a',
+             'ę' : 'e',
+             'ć' : 'c',
+             'ó' : 'o',
+             'ł' : 'l',
+             'ń' : 'n',
+             'ż' : 'z',
+             'ź' : 'z',
+             'ś' : 's'}
+
+# 1. sprawdź pliki
+to_rapair = check(REGEX)
+
+# na wszelki wypadek - gdyby nazwa samego skryptu naprawiajacego byla niepoprawna
+# wtedy wyklucz z listy
+if os.path.basename(__file__) in to_rapair:
+    to_rapair.remove(os.path.basename(__file__))
+
+# 2. utwórz nowe, poprawne nazwy
+translated = translate(to_rapair, DICT_TRANSLATE, REGEX)
+
+answer = input('\nCzy naprawic pliki? [tak - jesli jestes pewien]: ')
+answer = answer.lower().strip()
 if answer == 'tak':
 
-    # na wszelki wypadek - gdyby nazwa samego skryptu naprawiajacego byla niepoprawna
-    # wtedy wyklucz z listy
-    if os.path.basename(__file__) in to_rapair:
-        to_rapair.remove(os.path.basename(__file__))
-
-    repair(to_rapair)
+    # 3. napraw pliki
+    repair(to_rapair,translated)
 else:
     print('Dziękuję. Do zobaczenia.')
-
-
-
-
